9e47d4a7a3e1ba4d7a9a7b0c9918b2bc13932046
MagnusCardell <cardell.magnus@gmail.com>
2018-02-12 17:59:09 -0600

    hw B done

--- hw2/snippet1.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
- 
- int main(){
-     int x = 3;
-     int y = 5;
-     int z = y-3;
- }
---
+++ hw2/snippets.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+ 
+ int main(){
+     int x = 3;
+     int y = 5;
+     int z = y-3;
+ }
+
+int main(){
+    for(int i=0; i<=199; i+=2){
+        int x = 3;
+        x = 2*2;
+    }
+}
