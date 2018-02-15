93cfcf9d2eb119cad8cb205145dfb9ab94b5957b
MagnusCardell <cardell.magnus@gmail.com>
2018-02-14 21:48:54 -0600

    hw3 done

--- hw3/snippet4.cpp
+++ hw3/snippet4.cpp
@@ -1,16 +1,21 @@
 #include<iostream>
 #include<math.h>
+#include"../hw1/elapsed_time.h"
+
 using namespace std;
 
 int main(){
     int n = 100;
     int x;
+    start_timer(); // this function is from elapsed_time.h
 
     for ( int i = 0; i < n; ++i ) {
       for ( int j = 0; j < i; ++j ) {
         x = 32*sin(.32);
       }
     }
-
+    
+    double cycles = elapsed_time();  // also from elapsed_time.h
+    cout << " Total cycles: " << cycles << endl;
     return 0;
 }
