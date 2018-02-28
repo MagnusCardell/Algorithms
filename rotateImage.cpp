0587e89014f4063eb9488396835a057d1133eb3f
MagnusCardell <cardell.magnus@gmail.com>
2018-02-27 21:03:27 -0600

    fixed memory leak

--- rotateImage.cpp
+++ rotateImage.cpp
@@ -7,17 +7,18 @@ void rotateImage( int **im, int N ){
             int temp = im[i][j];
             im[i][j] = im[j][N-i];
             im[j][N-i] = temp;
-            cout<<temp;
+            cout<<temp<<" ";
         }
         cout<<"\n";
     }
+    //delete [] im;
 }
 
 int main(){
     int N = 4;
     int **image = new int*[N];
     for (int i =0; i<N; ++i){
-        *image[i] = i;
+        image[i] = new int[N];
     }
     for (int i =0; i<N; ++i){
         for( int j=0; j<N; ++j){
@@ -28,4 +29,5 @@ int main(){
     }
 
     rotateImage(image, N);
+    delete[] image;
 }
