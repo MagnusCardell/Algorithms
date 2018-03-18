0c0a2d92e5f12eb214364b144951f083212f194a
MagnusCardell <cardell.magnus@gmail.com>
2018-03-17 19:40:28 -0500

    datastructure done for graph reader

--- exercises/hw16/graph_reader.cpp
+++ exercises/hw16/graph_reader.cpp
@@ -2,40 +2,117 @@
 #include<iostream>
 #include<fstream>
 using namespace std;
- 
-vector<string> split(const string &line, const char *delim = "\t")
+
+struct Graph{
+    vector<struct Node> tree;
+    Node* root;
+
+    Graph(Node &r);
+    void addRoot(Node *r);
+    Node *getRoot();
+};
+
+Graph::Graph(Node &r){
+    //this->root = r;
+    tree.push_back(r);
+}
+
+void Graph::addRoot(Node *r){
+    this->root =r;
+}
+Node* Graph::getRoot(){
+    return root;
+}
+
+struct Node{
+    vector<Node> neighbours; //TODO: set to ID's to save memory
+    int val;
+
+    Node(){};
+    Node(int val);
+    void addNeighbour(Node &n);
+    void addNeighbour(int n);
+    void setVal(int n);
+    int getVal();
+    vector<Node> getNeighbours();
+    bool isLeaf();
+    bool operator==(Node &b);
+};
+
+Node::Node(int v){
+    this->val = v;
+}
+
+void Node::addNeighbour(Node &n){
+    this->neighbours.push_back(n);
+    return;
+}
+
+void Node::addNeighbour(int n){
+    Node temp(n);
+    this->neighbours.push_back(temp);
+}
+
+void Node::setVal(int n){
+    this->val = n;
+}
+
+int Node::getVal(){
+    return this->val;
+}
+
+vector<Node> Node::getNeighbours(){
+    return this->neighbours;
+}
+
+bool Node::isLeaf(){
+    return this->neighbours.size()==0;
+}
+
+bool Node::operator==(Node &b){
+    return b.val==this->val;
+}
+
+vector<string> split(const string &line, const char *delim = " \t")
 {
   vector<string> word;
   size_t loc0, loc1;
-  loc0 = line.find_first_not_of(delim); //location of non-delim word
+  loc0 = line.find_first_not_of(delim); //location of non-delim char
   if ( loc0 == string::npos ) return word; //if initial position=newline - return empty vector
-  loc1 = line.find_first_of(delim, loc0); // else find first position of this first non-newline char
+  loc1 = line.find_first_of(delim, loc0); // else find first position of delimiter after char
   while ( loc1 != string::npos ) { //while position != newline
-    cout<<"new delim"<<endl;
     word.push_back( line.substr(loc0, loc1-loc0) ); //get string between first char and delimiter
-    loc0 = line.find_first_not_of(delim, loc1);
-    if ( loc0 == string::npos ) break;
-    loc1 = line.find_first_of(delim, loc0);
+    loc0 = line.find_first_not_of(delim, loc1); // next first char after current delimiter
+    if ( loc0 == string::npos ) break; //this is newline, exit
+    loc1 = line.find_first_of(delim, loc0); //find next delimiter, repeat
   }
-  if ( loc0 != string::npos && loc1 == string::npos ) // one more
+  if ( loc0 != string::npos && loc1 == string::npos )
     word.push_back( line.substr(loc0, line.size()-loc0) );
   return word;
 }
- 
+
+void testDIMAC(vector<string> &word){
+    for ( unsigned i=0,i_end=word.size(); i<i_end; ++i )
+        cout << "," << word[i];
+    cout << endl;
+    return;
+}
+
 int main()
 {
-  ifstream f("graph.txt");
-  string line;
+    ifstream f("graph.txt");
+    vector <string> DIMAC;
 
-  while ( getline(f, line) ) {
-    // split the line by specific delimiter
-    vector<string> word = split(line);
-    cout << "Line:";
+    Node root;
+    Graph tree(root);
 
-    for ( unsigned i=0,i_end=word.size(); i<i_end; ++i )
-      cout << "," << word[i];
-
-    cout << endl;
-  }
-  f.close();
+    string line;
+    while ( getline(f, line) ) {
+        vector<string> word = split(line);
+        //testDIMAC(word);
+        DIMAC.push_back(word[0]);
+        
+        
+    }
+    f.close();
 }
