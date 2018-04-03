773091c7c51aaaebe9ced951e92957a39a6f32fc
MagnusCardell <cardell.magnus@gmail.com>
2018-04-02 22:47:00 -0500

    book exercise and dijkstra progress

--- exercises/hw17/dijkstra.cpp
+++ exercises/hw17/dijkstra.cpp
@@ -8,53 +8,36 @@ using namespace std;
 //typedef struct Edge Edge;
 
 struct Node{
+    string name;
     Node *parent;
-    int name;
-    int val;
-    bool visited = false;
-    vector<Node> neighbours;
-    Node(){};
-    Node(int i);
-    void add_neighbour(Node to, int len);
+    int dist = INF;
+    bool visited;
+    map<string, vector<int> > neighbours;
 
+    Node(string n){name = n;};
+    void add_neighbours(string to, int len);
 };
-Node::Node(int i){
-    name = i;
-    val = 0; //Root is always 0
+
+void Node::add_neighbours(string to, int len){
+    if(neighbours.count(to) <= 0){
+        cout<<"excisting key"<<endl;
+        neighbours[to].push_back(len);
+    }
+    else{
+        cout<<"new key"<<endl;
+        neighbours[to].push_back(len);
+    }
 }
-void Node::add_neighbour(Node to, int len){
-    neighbours[len]= to;
-}
+
 /*
-struct Edge{
-    Node from;
-    Node to;
-    int length;
-    
-    Edge(Node n1, Node n2, int l);
-};
-
-Edge::Edge(Node n1, Node n2, int l){
-    from = n1;
-    to = n2;
-    length = l;
-}
-*/
-
 struct Graph{
     Node root;
-    //vector<Edge> vertices;
     vector<Node> tree;
 
-    //void add(Edge e);
     void add(Node n);
     void add_root(Node n);
 };
-/*
-void Graph::add(Edge e){
-    vertices.push_back(e);
-}
-*/
+
 void Graph::add(Node n){
     tree.push_back(n);
 }
@@ -62,13 +45,12 @@ void Graph::add(Node n){
 void Graph::add_root(Node n){
     root = n;
 }
+*/
 
 void print_node(Node *n){
-    cout<<n->name<<": "<<n->val<<endl;
+    std::cout<<"Node length: "<< n->dist <<endl;
 }
-//void print_edge(Edge *e){
-//    cout<< e->from.name<<"-> "<<e->to.name<<": "<< e->length<<endl;
-//}
+/*
 void print_graph(Graph *g){
     cout<<"These are the nodes and their neighbours: "<<endl;
     for( int i=0, i_end=g->tree.size(); i<i_end; ++i){
@@ -80,75 +62,40 @@ void print_graph(Graph *g){
         }
         cout<<'\n';
     }
-/*
-    cout<<"These are all the vertice distances: "<<endl;
-    for( int i =0, i_end=g->vertices.size(); i<i_end; ++i){
-        //cout<<g->vertices[i].from.name<<endl;
-        print_edge(&g->vertices[i]);
-    }
-*/
+
 }
+*/
 
 //Construct method, to initialize a graph by hand
 //I am recreating the graph from the book p.111
-void construct_graph(Graph *G){
-    Node A(0);
-    Node B(1);
-    Node C(2);
-    Node D(3);
-    Node E(4);
+void construct_graph(Node *A){
+    Node B("B");
+    Node C("C");
+    Node D("D");
+    Node E("E");
 
-    //Edge t1(A, B, 4);
-    A.add_neighbour(B, 4);
-    //Edge t2(A, C, 2);
-    A.add_neighbour(2, 2);
-    //Edge t3(B, C, 1);
-    B.add_neighbour(2, 1);
-    //Edge t4(C, B, 3);
-    C.add_neighbour(1, 3);
-    //Edge t5(B, D, 2);
-    B.add_neighbour(3, 2);
-    //Edge t6(B, E, 3);
-    B.add_neighbour(4, 3);
-    //Edge t7(C, D, 4);
-    C.add_neighbour(3, 4);
-    //Edge t8(E, D, 1);
-    E.add_neighbour(3, 1);
-    //Edge t9(C, E, 5);
-    C.add_neighbour(4, 5);
-
-    G->add(A);
-    G->add(B);
-    G->add(C);
-    G->add(D);
-    G->add(E);
-    G->add_root(A);
-/*
-    G->add(t1);
-    G->add(t2);
-    G->add(t3);
-    G->add(t4);
-    G->add(t5);
-    G->add(t6);
-    G->add(t7);
-    G->add(t8);
-    G->add(t9);
-*/  
+    A->add_tree("B", 4);
+    A->add_tree("C", 2);
+    B.add_tree("C", 3);
+    B.add_tree("D", 2);
+    B.add_tree("E", 3);
+    C.add_tree("B", 1);
+    C.add_tree("D", 4);
+    C.add_tree("E", 5);
+    E.add_tree("D", 1);
     return;
 }
 
-//Node *find_smallest_to_visit(map<string, int> *d, map<string, bool> *v ){
-    
-//}
-
-void do_dijkstra( Graph *g){
+/*
+void do_dijkstra( Node *r){
     cout<<"Starting Dijkstra's shortest path algorithm"<<endl;
 
     // 1. Set all values to infinite
     for( int i=0, i_end=g->tree.size(); i<i_end; ++i){
-        g->tree[i].val = INF;
+        r->tree[i].val = INF;
         g->tree[i].parent = NULL;
     }
+    
 
     // 2. Create a lookup table 
     vector <Node*> queue;
@@ -192,28 +139,18 @@ void do_dijkstra( Graph *g){
         cout<<queue.size()<<endl;
         //Node *temp = dist[0];
     }
-    //print_graph(g);
-    //map<string, int> dist;
-    //dist[root->name] = 0;
-    //map<string, bool> visited;
-    //visited[root->name] = false;
-
-    //Node nodes[g->tree.size()];
-    //nodes[0] = *root;
-
-    //for(int i =0, i_end=g->tree.size(); i<i_end; ++i){
-    //    Node *temp = find_smallest_to_visit(&dist, &visited);
-    //}
 
     return;
 }
+*/
 int main(){
 
-    Graph g;
-    construct_graph(&g);
-    print_node(&g.root);
-    print_graph(&g);
+    Node root("A");
+    construct_graph(&root);
+    print_node(&root);
+    cout<<root.tree.size()<<endl;
+    //print_graph(&g);
 
-    do_dijkstra(&g);
+    //do_dijkstra(&root);
 
 }
--- exercises/hw18/README.md
+++ exercises/hw18/README.md
@@ -12,4 +12,6 @@ B. Exercises
 
     2.[C] Exercise 4.12, page 121.
 
+Starting at vertex v in graph G. Use DFS to find all cycles and save the shortest. Repeat this process with each neighbour until all verteces have been visited and used as root once. Time complexity would be V*(V*E) where V are the number of verteces abd E the total number of edges.
+
     3.[C] Solve the pouring water problem from a prior homework (the 3-bucket version) with the following additional constraint: we want to minimize the total amount of water that is poured out of any given container. That is, we will consider the distance between two nodes to be the amount of water that is poured out during that move. As output, show the sequence of nodes for a shortest path, as well as the total distance traveled from the start to the goal. Please submit the code via git and copy your output here.
