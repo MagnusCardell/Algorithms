cca4a84d708565834bd5062d515bb23e438ad070
Magnus Cardell <cardell.magnus@gmail.com>
2023-10-14 23:06:23 +0200

    simple hierachy counter

---
+++ python/employee_hierachy.py
@@ -0,0 +1,14 @@
+
+def calculate_employee_score(hierarchy: {int: [int]}, eid: int) -> int:
+    sum = 0
+    if( eid not in hierarchy):
+        return 1
+    else:
+        for x in hierarchy[eid]:
+            sum += calculate_employee_score(hierarchy, x)
+    sum += 1
+    print(eid, sum)
+    return sum
+
+assert calculate_employee_score({}, 1) == 1, "base"
+assert calculate_employee_score({123: [234, 345], 234: [456, 789], 345:[], 456:[], 789:[]}, 123) == 5, "example"
